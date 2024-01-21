bc17f69f1f87cc9c79fc196d31c16a094442322d
Kit <kitherman@gmail.com>
2024-01-21 10:09:39 -0500

    colorscheme changes

--- config/lua/nvim-0-init.lua
+++ config/lua/nvim-0-init.lua
@@ -1,11 +1,12 @@
 vim.g.mapleader = " "
-vim.o.tabstop=2
-vim.o.softtabstop=2
-vim.o.shiftwidth=2
+vim.o.tabstop=4
+vim.o.softtabstop=4
+vim.o.shiftwidth=4
 vim.o.expandtab = true
+vim.opt.ignorecase = true
 vim.o.smartindent=true
 vim.o.number=true
-vim.cmd('colorscheme 256_noir')
+vim.cmd('colorscheme orbital')
 vim.o.wrap=true
 vim.o.swapfile=false
 vim.o.history=500
