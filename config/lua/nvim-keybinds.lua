310aeba22e6f381712fbd55f0ece16692dd91f63
kit <kitherman@gmail.com>
2024-01-24 21:04:27 -0500

    keybind fixes

--- config/lua/nvim-keybinds.lua
+++ config/lua/nvim-keybinds.lua
@@ -1,18 +1,19 @@
 local opt = { noremap = true }
 
 -- paste improvements
-vim.api.nvim_set_keymap('x', '<leader>yy', '"+y', { noremap = true, silent = false})
-vim.api.nvim_set_keymap('v', '<leader>yy', '"+y', { noremap = true, silent = false})
+vim.api.nvim_set_keymap('x', '<leader>yy', '"+y', opt)
+vim.api.nvim_set_keymap('v', '<leader>yy', '"+y', opt)
 
-vim.api.nvim_set_keymap('x', '<leader>pp', '"+p', { noremap = true, silent = false})
-vim.api.nvim_set_keymap('v', '<leader>pp', '"+p', { noremap = true, silent = false})
-vim.api.nvim_set_keymap('n', '<leader>pp', '"+p', { noremap = true, silent = false})
+vim.api.nvim_set_keymap('x', '<leader>pp', '"+p', opt)
+vim.api.nvim_set_keymap('v', '<leader>pp', '"+p', opt)
+vim.api.nvim_set_keymap('n', '<leader>pp', '"+p', opt)
 
 -- Open terminal, run nix run, and switch back to the original window
-vim.api.nvim_set_keymap('n', '<Leader>ww', ':belowright 15sp <CR> :terminal nix build <CR> :wincmd k <CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', '<Leader>ww', ':w <CR> :belowright 15sp <CR> :terminal nix build <CR> :wincmd k <CR>', opt)
+vim.api.nvim_set_keymap('n', '<Leader>wd', ':w <CR> :belowright 15sp <CR> :terminal nix develop <CR> :wincmd k <CR>', opt)
 
 -- Window navigation
-vim.api.nvim_set_keymap('n', '<C-h>', ':wincmd h <CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-j>', ':wincmd j <CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-k>', ':wincmd k <CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-l>', ':wincmd l <CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-h>', ':wincmd h <CR>', opt)
+vim.api.nvim_set_keymap('n', '<C-j>', ':wincmd j <CR>', opt)
+vim.api.nvim_set_keymap('n', '<C-k>', ':wincmd k <CR>', opt)
+vim.api.nvim_set_keymap('n', '<C-l>', ':wincmd l <CR>', opt)
