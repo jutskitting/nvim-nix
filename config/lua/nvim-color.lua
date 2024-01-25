7acaba9a3b0729eaee5e1e328ec4e711f82f8cf6
kit <kitherman@gmail.com>
2024-01-24 19:57:03 -0500

    color adjustments

--- config/lua/nvim-color.lua
+++ config/lua/nvim-color.lua
@@ -2,4 +2,5 @@
 vim.cmd("colorscheme orbital")
 
 -- Override the String highlight group
-vim.api.nvim_set_hl(0, "String", { ctermbg = "NONE", guibg = "NONE", guifg = "#1B4F72" })
+-- vim.api.nvim_set_hl(0, "String", { ctermbg = "NONE", bg = "NONE", fg = "#2c81ba" })
+vim.api.nvim_set_hl(0, "String", { ctermbg = "NONE", bg = "NONE", fg = "#119dae" })
